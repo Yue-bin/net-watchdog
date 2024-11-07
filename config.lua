1180c891ff9f1b94c301f405b7bc7d960dde7364
Yue-bin <yuebinjiang@qq.com>
2024-11-07 18:50:04 +0800

    ✨ feat(main,config): 主体和对应的配置基本实现完毕

--- config.lua
+++ config.lua
@@ -1,4 +1,29 @@
 --[[
     config.lua - configuration file
+    本文件中的各种函数中可以调用从mods中require的模块的函数以实现各种模块化的功能
 ]]
 
+CONFIG = {}
+
+-- sleep的间隔, 单位秒
+CONFIG.interval = 5
+
+-- sleep函数的实现
+CONFIG.sleep = function(seconds)
+    os.execute("sleep " .. seconds)
+end
+
+-- 主循环的退出条件
+CONFIG.exit = function()
+    return false
+end
+
+-- 触发器
+CONFIG.trigger = function()
+    return true
+end
+
+-- 触发器触发的动作
+CONFIG.action = function()
+    print("triggered", os.date())
+end
--- net-watchdog-op.lua
+++ net-watchdog-op.lua
@@ -4,3 +4,13 @@
 
 -- 加载配置文件
 dofile("config.lua")
+
+-- 主循环
+while not CONFIG.exit() do
+    -- 如果触发器触发, 则执行动作
+    if CONFIG.trigger() then
+        CONFIG.action()
+    end
+    -- 等待一段时间
+    CONFIG.sleep(CONFIG.interval)
+end
